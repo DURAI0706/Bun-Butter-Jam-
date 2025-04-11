ec9c176f43e484af77bea3c130dac84dd6020c71
R DURAISAMY <112633012+DURAI0706@users.noreply.github.com>
2025-04-11 22:27:58 +0530

    Update login.py

--- login.py
+++ login.py
@@ -161,15 +161,11 @@ def show_login_page():
     }
 
     .centered-login {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      text-align: center;
-      padding: 20px;
-      border: 1px solid #ccc;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      background-color: white;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
     }
 
     /* Glass-style login button */
