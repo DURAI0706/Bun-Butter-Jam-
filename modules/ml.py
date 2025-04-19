b7bcf97673b59418636304f9c135079fade0247d
R DURAISAMY <112633012+DURAI0706@users.noreply.github.com>
2025-04-19 22:38:06 +0530

    Update ml.py

--- modules/ml.py
+++ modules/ml.py
@@ -10,8 +10,6 @@ from dateutil import parser
 from mlxtend.frequent_patterns import apriori, association_rules
 
 # Set page config
-st.set_page_config(page_title="Retail Analytics Dashboard")
-
 # Sidebar for navigation
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Sales Forecasting", "Market Basket Analysis"])
